21faf36f2cb6866fc2e25005fdecd1d69d2d43ef
gabriel-molinari <molinarigabriel22@gmail.com>
2025-08-31 15:38:24 -0300

    Update TP Unidad 3 Condicionales - Gabriel Molinari.py
    Cambio en comentario

--- TP Unidad 3 Condicionales - Gabriel Molinari.py
+++ TP Unidad 3 Condicionales - Gabriel Molinari.py
@@ -1,4 +1,4 @@
-#Trabajo Practico 2 - Condicionales - Gabriel Molinari
+#Trabajo Practico Unidad 3 - Condicionales - Gabriel Molinari
 
 #1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años,
 #deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”.
